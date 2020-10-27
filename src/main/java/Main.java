4a1918cfcdd217c1004f36860455f83ecc001505
marinacuzub <73469781+marinacuzub@users.noreply.github.com>
2020-10-27 21:57:56 +0200

    Added several methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,4 +11,21 @@ public class Main {
                 ", b=" + b +
                 '}';
     }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
 }
